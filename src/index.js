40a86964d9b5296f0263902cd8a3f7c1f005cec9
Kanstantsin Kiyko <xfozzyx@gmail.com>
2018-02-21 17:35:00 +0300

    al tests pass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+  let multipliersOfFive = [];
+  let multiplier = 5;
+  while (multiplier <= number) {
+    multipliersOfFive.push(multiplier);
+    multiplier *= 5;
+  }
+  const reducer = (sum, element) => sum + Math.floor(number / element);
+  return multipliersOfFive.reduce(reducer, 0);
+};
